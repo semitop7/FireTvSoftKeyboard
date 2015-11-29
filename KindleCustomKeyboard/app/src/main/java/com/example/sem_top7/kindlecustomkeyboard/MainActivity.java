bad6b32cc207e4c8544813870fe3dfd461e3039f
sem_top7 <semitop7@gmail.com>
2015-11-29 15:34:56 +0200

    test AlertDialog

--- KindleCustomKeyboard/app/src/main/java/com/example/sem_top7/kindlecustomkeyboard/MainActivity.java
+++ KindleCustomKeyboard/app/src/main/java/com/example/sem_top7/kindlecustomkeyboard/MainActivity.java
@@ -22,7 +22,6 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
